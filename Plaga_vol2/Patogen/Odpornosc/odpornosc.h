23608d9b58edf773074ce0c990421b5a0aaa6662
mkorzonek <70174016+mkorzonek@users.noreply.github.com>
2020-08-25 12:53:22 +0200

    Create odpornosc.h

---
+++ Plaga_vol2/Patogen/Odpornosc/odpornosc.h
@@ -0,0 +1,18 @@
+#ifndef ODPORNOSC_H
+#define ODPORNOSC_H
+
+
+class Odpornosc
+{
+public:
+    Odpornosc();
+    int temp_rozw_max;
+    int temp_rozw_min;
+    int temp_latencji_max;
+    int temp_latencji_min;
+    int temp_zycia_max;
+    int temp_zycia_min;
+    int odp_leki;
+};
+
+#endif // ODPORNOSC_H
