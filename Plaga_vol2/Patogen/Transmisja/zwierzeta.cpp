5a9cdc0c4d1a0707de5fee603d6995f04a1f539a
mkorzonek <70174016+mkorzonek@users.noreply.github.com>
2020-08-25 12:52:53 +0200

    Create zwierzeta.cpp

---
+++ Plaga_vol2/Patogen/Transmisja/zwierzeta.cpp
@@ -0,0 +1,16 @@
+
+#include "zwierzeta.h"
+
+Zwierzeta::Zwierzeta()
+{
+
+    gryzonie_1=0;
+    gryzonie_2=0;
+    owady_1=0;
+    owady_2=0;
+    ptaki_1=0;
+    ptaki_2=0;
+    zw_gospodarcze_1=0;
+    zw_gospodarcze_2=0;
+
+}
