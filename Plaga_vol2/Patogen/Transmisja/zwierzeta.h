c081df9208af041925c2380e7eca3b621fa152c7
mkorzonek <70174016+mkorzonek@users.noreply.github.com>
2020-08-25 12:53:45 +0200

    Create zwierzeta.h

---
+++ Plaga_vol2/Patogen/Transmisja/zwierzeta.h
@@ -0,0 +1,21 @@
+#ifndef TRANS_ZWIERZETA_H
+#define TRANS_ZWIERZETA_H
+
+
+class Zwierzeta
+{
+
+public:
+    friend class Widget;
+    bool gryzonie_1;
+    bool gryzonie_2;
+    bool owady_1;
+    bool owady_2;
+    bool ptaki_1;
+    bool ptaki_2;
+    bool zw_gospodarcze_1;
+    bool zw_gospodarcze_2;
+    Zwierzeta();
+};
+
+#endif // TRANS_ZWIERZETA_H
